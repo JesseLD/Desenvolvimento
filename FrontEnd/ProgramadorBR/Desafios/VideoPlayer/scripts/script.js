246bb24078301c9d40dea632869a09fde28e3dfc
Jessé Oliveira <82299456+JesseLD@users.noreply.github.com>
2022-12-02 10:38:17 -0300

    terminar

--- FrontEnd/ProgramadorBR/Desafios/VideoPlayer/scripts/script.js
+++ FrontEnd/ProgramadorBR/Desafios/VideoPlayer/scripts/script.js
@@ -1,21 +1,39 @@
 let play = document.querySelector("span.play");
 let pause = document.querySelector("span.pause");
-let video = document.querySelector("div.container");
+let video = document.querySelector("video.video");
 let cts = document.querySelector("div.buttons");
+var second_container = document.querySelector("div.second-container");
+let btn = document.querySelectorAll("span.btn")[0];
+let span = document.getElementsByTagName("span");
+
+arr=["1","2","3"]
+
 
 
 play.addEventListener("click",start);
+play.addEventListener("click",pause);
+
 
 video.addEventListener("mousemover",ct);
 
 function start(){
-  this.innerText="pause"
   video.play();
+  this.style.opacity= "0"
+
 }
-function stop(){
+
+function pause(){
   video.pause();
 }
+
 function ct(){
   cts.style.display="inline";
 }
+function m_hover(){
+  second_container.style.backgroundColor="yellow";
+  
+}
+  
+  
+
 
